019be55084feaf5c7eaa425f48b415bfdd911e62
Jeremy Gilbert <jgilbert@ausice.net>
2016-07-09 12:19:42 -0400

    Fixes for rip

--- run-arp.pl
+++ run-arp.pl
@@ -3,10 +3,13 @@
 my $t = time();
 
 open TOOL, "arp -a|" or die;
+my $x = `date`;
+chomp $x;
+
 while( <TOOL> )
 {
 	# print $_;
-	my @a = 	/^([^\s]+)\s+\(([\d\.]+)\)\s+at\s+([^\s]+).*+on\s+([^\s]+)/;
-	print join "\t", ( 'ARP', $t, @a ) ;
+	my @a = /^([^\s]+)\s+\(([\d\.]+)\)\s+at\s+([^\s]+)/; # .*+on\s+([^\s]+)/;
+	print join "\t", ( 'ARP', $t, $x, @a ) ;
 	print "\n";
 }
--- run-arpscan.pl
+++ run-arpscan.pl
@@ -1,13 +1,16 @@
 #!/usr/bin/perl
 
 my $t = time();
+my $x = `date`;
+chomp $x;
 
 open TOOL, "arp-scan --local|" or die "$!";
 while( <TOOL> )
 {
-	# print $_;
+    # print $_;
+    next if /DUP/;
 	my @a = /^([\d\.]{6,})\s+([^\s]+)\s+(.+)$/;
 	next unless @a;
-	print join "\t", ( 'ARPSCAN', $t, @a ) ;
+	print join "\t", ( 'ARPSCAN', $t, $x, @a );
 	print "\n";
 }
