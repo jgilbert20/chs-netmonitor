a1e40254e0f6370eb4d954ae64cc607e7c45f40d
Jeremy Gilbert <jgilbert@ausice.net>
2016-07-11 10:37:05 -0400

    Updated version

--- runall.sh
+++ runall.sh
@@ -1,9 +1,15 @@
+#!/bin/sh
+
+# Add this to pi's crontab
+#*/5 *  *   *   *     /home/pi/src/chs-netmonitor/runall.sh
+
+cd /home/pi/src/chs-netmonitor/
 scp -P 27182 root@192.168.1.2:last-chs2-assoc .
 scp root@192.168.1.8:last-chs8-assoc .
 # put mikrotik password in ~/pass
 sshpass -f ~/pass ssh admin@192.168.1.4 interface wireless registration-table print > last-chs4-assoc
 arp -a > last-arp
 avahi-browse -a --resolve -c -p > last-avahi-browse
-# nmap -A -T4 192.168.1.0/24 > last-nmap
+nmap -A -T4 192.168.1.0/24 > last-nmap
 perl run-nmap.pl >> strutured-log.tsv
 
